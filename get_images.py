3fa30203501ed08cf074b4ab0771e6d6febc516c
FluxusCrow <pascal.kroh@web.de>
2022-11-01 21:10:53 +0100

    Creates folder images_original automatically and checks if images are already inside

--- get_images.py
+++ get_images.py
@@ -3,6 +3,15 @@ Catalogue file. Images are saved under data/images/ as png.
 '''
 
 import fitz
+import os
+import sys
+
+# change working directory to directory of this file
+os.chdir(sys.path[0])
+
+# create folder "data/images_original" if not existing already
+if not "images_original" in os.listdir("data"):
+    os.mkdir("data/images_original")
 
 # get data
 catalogue = "data/orca_catalogue_2019.pdf"
@@ -13,5 +22,6 @@ for page in range(22,183):
     for img in pdf.get_page_images(page):
         xref = img[0]
         picture = fitz.Pixmap(pdf, xref)
-        picture.save("data/images/p%s-%s.png" % (page, xref), output="png")
-        picture = None
+        if not "p%s-%s.png" % (page, xref) in os.listdir("data/images_original"):
+            picture.save("data/images_original/p%s-%s.png" % (page, xref), output="png")
+            picture = None
